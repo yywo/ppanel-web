6024454d57d68ca96f0e72e1866188a2f6f6f3df
web@ppanel <web@ppanel.dev>
2024-11-21 14:51:30 +0700

    ✨ feat(tos): Display data

--- apps/user/app/(main)/tos/page.tsx
+++ apps/user/app/(main)/tos/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-
+import { getTos } from '@/services/common/common';
 import { Markdown } from '@repo/ui/markdown';
 
-export default function Page() {
+export default async function Page() {
+  const { data } = await getTos();
   return (
     <div className='container py-8'>
-      <Markdown>隐私协议</Markdown>
+      <Markdown>{data.data?.tos_content || ''}</Markdown>
     </div>
   );
 }
