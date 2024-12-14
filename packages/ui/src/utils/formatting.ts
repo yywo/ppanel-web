bbc2da05f0b5ea948707a84a766a30adea6ca0ce
web@ppanel <web@ppanel.dev>
2024-12-14 21:49:48 +0700

    🐛 fix(config): FormatBytes

--- packages/ui/src/utils/formatting.ts
+++ packages/ui/src/utils/formatting.ts
@@ -2,11 +2,12 @@ import { intlFormat } from '@shadcn/ui/lib/date-fns';
 
 export function formatBytes(bytes: number) {
   if (bytes === 0) return '0 B';
-  const k = 1024, // or 1000
-    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-    i = Math.floor(Math.log(bytes) / Math.log(k));
 
-  return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+  const k = 1024;
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return (bytes / Math.pow(k, i)).toFixed(2) + ' ' + sizes[i];
 }
 
 export function formatDate(date?: Date | number, showTime: boolean = true) {
